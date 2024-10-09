d0bbf0768361af0e6052e6a17b68f7d6257351bd
inesrhh <123432531+inesrhh@users.noreply.github.com>
2024-10-08 19:32:04 -0700

    blue wavy box

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -29,8 +29,8 @@ const FlatMesh = () => {
   return (
     <mesh ref={mesh} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={1.5}>
       <planeGeometry args={[1, 1, 45, 45]} />
-      {/* <meshBasicMaterial color="red" /> */}
-      {/* <boxGeometry args={[1, 1, 1]}/> */}
+      {/* <meshBasicMaterial color="red" />  */}
+      <boxGeometry args={[0.1, 0.1, 0.1]}/>
       <shaderMaterial
         fragmentShader={fragmentShader}
         vertexShader={vertexShader}
--- src/fragmentShader.glsl
+++ src/fragmentShader.glsl
@@ -1,7 +1,7 @@
 varying vec2 vUv;
 
 vec3 colorA = vec3(0.0,0.0,1.0);
-vec3 colorB = vec3(1.000,0.0,0.0);
+vec3 colorB = vec3(0.2,0.9,1.0);
 
 void main() {
   // "Normalizing" with an arbitrary value
