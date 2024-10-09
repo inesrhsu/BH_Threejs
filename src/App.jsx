6f63067c16fd464980e9fc186dabac8440aca773
inesrhh <123432531+inesrhh@users.noreply.github.com>
2024-10-08 19:08:53 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Scene from './Scene'; // Ensure this is the correct path
+
+function App() {
+  return (
+    <main>
+      <Scene />
+    </main>
+  );
+}
+
+export default App;
+
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Make sure you're importing App correctly
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
