2e30cf0ad0deffbc3fabae34be6af63a2e3ac380
inesrhh <123432531+inesrhh@users.noreply.github.com>
2024-10-11 16:17:01 -0700

    nine waves one with rotation according to time

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,8 +1,8 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
+import { useMemo, useRef, useEffect } from "react";
 import { Color } from "three";
-import './App.css';
+// import './App.css';
 
 import vertexShader from './vertexShader.glsl';
 import fragmentShader from './fragmentShader.glsl';
@@ -12,6 +12,7 @@ import fragmentShader from './fragmentShader.glsl';
 const FlatMesh = () => {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
+  const meshes = [];
 
   const uniforms = useMemo(
     () => ({
@@ -21,17 +22,10 @@ const FlatMesh = () => {
     }), []
   );
 
-  useFrame((state) => {
-    const { clock } = state;
-    mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
-  });
-
-  const meshes = [];
-
-  for (let x = -2; x <= 2; x+=2){
+ for (let x = -2; x <= 2; x+=2){
     for (let y=-2; y<= 2; y+=2){
       meshes.push(
-        <mesh ref={mesh} position={[x, y, -x]} rotation={[-Math.PI / 8, Math.PI / 6, 0]} scale={1.5}>
+        <mesh key={`${x}+${y}`} ref={mesh} position={[x, y, -x]} rotation={[-Math.PI/2.5, Math.PI / 6, 0]} scale={1.5}>
           <planeGeometry args={[1, 1, 45, 45]} />
           <shaderMaterial
           fragmentShader={fragmentShader}
@@ -43,32 +37,34 @@ const FlatMesh = () => {
       }
     }
 
+  useFrame((state) => {
+    const { clock } = state;
+    const elapsedTime = clock.getElapsedTime();
+    mesh.current.material.uniforms.u_time.value = elapsedTime;
+    
+     mesh.current.rotation.set(-Math.PI/((Math.sin(elapsedTime/2)*2+4)), Math.PI / 6, 0);
+  });
+
+  
+   
+  
+
 
   return (
-    <>
-    
-    {meshes}
-    {/* <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={1.5}>
-        <sphereGeometry args={[0.12]}/>
-    </mesh> */}
-    </>
+    <>{meshes}</>
   );
 };
 
 const Scene = () => {
   return (
     <section className="canvas-container">
-      <Canvas className="canvas" dpr={[1, 2]} camera={{ position: [10, 0, 10], rotation:[0,0,0], fov: 45 }} shadows>
+      <Canvas className="canvas" dpr={[1, 2]} camera={{ position: [7, 0, 7], rotation:[0,0,0], fov: 45 }} shadows>
         <color attach="background" args={["#FFC0CB"]} />
         <ambientLight intensity={0.7} /> 
         <directionalLight position={[5, 5, 5]} intensity={1} castShadow/>
 
-        {/* <mesh castShadow receiveShadow>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="red" />
-        </mesh> */}
         <FlatMesh />
-        {/* <axesHelper /> */}
+
         <OrbitControls />
       </Canvas>
     </section>
